1b1d74087504cdf612fd1c937b6d0ff7b8013f06
Khukhlaev <khukhlaev.so@phystech.edu>
2019-11-22 13:58:16 +0300

    Changed skeleton of the class Level

--- classes/class_Level.py
+++ classes/class_Level.py
@@ -3,11 +3,12 @@ from objects.class_Platform import Platform
 
 class Level:
 
-    def __init__(self, canvas, length, platforms, person):
+    def __init__(self, canvas, length, platforms, person, enemies):
         self.canvas = canvas
         self.length = length  # Length of the level in coordinates units??
         self.platforms = platforms
         self.person = person
+        self.enemies = enemies
         self.end_level = False
 
     def draw_all(self, velocity):
@@ -18,7 +19,9 @@ class Level:
 
     def cadre(self):
         self.change_coords()
-        self.draw_all()
+        self.draw_all(2)
+
+    def check_for_end(self):
 
     def game(self):
         while not self.end_level:
