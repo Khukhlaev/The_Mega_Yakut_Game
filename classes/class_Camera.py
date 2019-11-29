3f6f77a7f9bcc909241321356572a97201bdb088
Khukhlaev <khukhlaev.so@phystech.edu>
2019-11-29 14:15:56 +0300

    New amazing camera (OMG it'so freaking beauty)

--- classes/class_Camera.py
+++ classes/class_Camera.py
@@ -22,20 +22,22 @@ class Camera:
                                                       self.player.x + self.player.width,
                                                       self.player.y + self.player.height, fill="Red")
         self.enemies = enemies
+        self.level_x = 0
 
-    def need_center(self):
-        """Return True if we need to center model of the player
-                  False if we don't need this
+    def player_on_center(self):
+        """Return False if we need to center model of the player
+                  True if we don't need this (player is already on center)
         """
-        if self.canvas.coords(self.player_id)[0] > self.canvas.winfo_width() / 2:
+        if self.canvas.coords(self.player_id)[2] < self.canvas.winfo_width() / 2 or self.player.vx < 0:
             return False
         return True
 
     def update(self):
         coordinates = self.canvas.coords(self.player_id)
-        if self.need_center():
+        if self.player_on_center():
             for platform in self.platforms_id:
-                self.canvas.move(platform, - self.player.vx, 0)
+                self.canvas.move(platform, - self.player.vx, 0)  # Move all platforms on the canvas to center player
+            self.level_x += self.player.vx
         else:
             self.canvas.move(self.player_id, self.player.vx, self.player.vy)
         self.canvas.update()
