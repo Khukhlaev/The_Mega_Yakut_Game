91be34ec1cce9e2a5b7563a23af31e416a3f364f
Khukhlaev <khukhlaev.so@phystech.edu>
2019-11-29 14:37:23 +0300

    Fix some bugs with camera

--- classes/class_Camera.py
+++ classes/class_Camera.py
@@ -37,6 +37,7 @@ class Camera:
         if self.player_on_center():
             for platform in self.platforms_id:
                 self.canvas.move(platform, - self.player.vx, 0)  # Move all platforms on the canvas to center player
+            self.canvas.move(self.player_id, 0, self.player.vy)
             self.level_x += self.player.vx
         else:
             self.canvas.move(self.player_id, self.player.vx, self.player.vy)
