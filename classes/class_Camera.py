639194259aa8ab524855287d159c76ebba77824f
Alexey Legoshin <Legoshin.AD@phystech.edu>
2019-12-09 18:03:23 +0300

    Added graphical player and its hitbox as white rectangle. TODO: change this rectangle height and make a fullscreen to end the idea process. We need to have a permanent windows parameters. Also there is a problem, that down part of the hitbox cover the heighest part of platform. Maybe may be fixed by plusing some pixels to platform height. AND YOU WILL BE AS HAPPY AS YOU CAN IMAGINE! SPRITES WITHOUT BACKGROUND ARE BACKGROUND FREE IN TKINTER! THIS IS THE VICTORY, FRIENDS!

--- classes/class_Camera.py
+++ classes/class_Camera.py
@@ -1,6 +1,8 @@
 """This class will operate with canvas coordinate system (x, y), point (0,0) on the left top side of the window
 """
 
+from PIL import Image, ImageTk
+
 
 class Camera:
     def __init__(self, canvas, player, platforms, enemies):
@@ -18,9 +20,14 @@ class Camera:
                                              platform.x + platform.width,
                                              platform.y + platform.height, fill="Green")
             )
-        self.player_id = self.canvas.create_rectangle(self.player.x, self.player.y,
-                                                      self.player.x + self.player.width,
-                                                      self.player.y + self.player.height, fill="Red")
+        img = Image.open("graphics/sprites/player_static_right.png")
+        self.player_hit_box = self.canvas.create_rectangle(self.player.x, self.player.y,
+                                                           self.player.x + self.player.width,
+                                                           self.player.y + self.player.height, outline="white",
+                                                           fill="white")
+        render = ImageTk.PhotoImage(img)
+        self.player_id = canvas.create_image(self.player.x, self.player.y, image=render)
+        self.show_sprite = render
         self.enemies = enemies
         self.level_x = 0
 
@@ -28,21 +35,22 @@ class Camera:
         """Return False if we need to center model of the player
                   True if we don't need this (player is already on center)
         """
-        if self.canvas.coords(self.player_id)[2] < self.canvas.winfo_width() / 2 or self.player.vx < 0:
+        if self.canvas.coords(self.player_hit_box)[2] < self.canvas.winfo_width() / 2 or self.player.vx < 0:
             return False
         return True
 
     def update(self):
-        coordinates = self.canvas.coords(self.player_id)
+        coordinates = self.canvas.coords(self.player_hit_box)
         if self.player_on_center():
             for platform in self.platforms_id:
                 self.canvas.move(platform, - self.player.vx, 0)  # Move all platforms on the canvas to center player
+            self.canvas.move(self.player_hit_box, 0, self.player.vy)  # Move player on the canvas y coordinate
             self.canvas.move(self.player_id, 0, self.player.vy)  # Move player on the canvas y coordinate
             self.level_x += self.player.vx
         else:
+            self.canvas.move(self.player_hit_box, self.player.vx, self.player.vy)
             self.canvas.move(self.player_id, self.player.vx, self.player.vy)
         if self.player.push_on_platform:
             self.player.push_on_platform = False
             self.player.vy = 0
         self.canvas.update()
-
