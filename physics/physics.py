cff21f080b9f86377f6ed2460b60fda56c7c66e5
Yaushev Mikhail <yaushev.mikhail@mail.ru>
2019-12-12 18:54:01 +0300

    Add some comments

--- physics/physics.py
+++ physics/physics.py
@@ -2,6 +2,7 @@ g = 0.1667  # gravity acceleration per frame
 
 
 def gravity(vy, on_platform):
+    """Add a gravity effect if the player is not on the platform"""
     global g
     if not on_platform:
         vy += g
