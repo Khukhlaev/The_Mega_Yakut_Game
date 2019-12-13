a391f71a5696c4b68d3c392a36999b40dc43915a
Alexey Legoshin <Legoshin.AD@phystech.edu>
2019-12-13 11:32:14 +0300

    Process of adding logic for enemies. Fixed some functions. I'm a punk. PEP8 is forbidden.

---
+++ ai/class_Dumb.py
@@ -0,0 +1,17 @@
+class Dumb:
+
+    def __init__(self, x, y, width, height):
+        self.x = x  # It is coordinates in natural coordinate system (not canvas coordinate system)
+        self.y = y
+        self.x_abstract = x
+        self.y_abstract = y
+        self.width = width
+        self.height = height
+        self.vx = 0
+        self.vy = 0
+        self.on_platform = False
+        self.platform_beside = False
+
+
+    def move(self):
+        if self.on_platform and self.x + self.vx: pass
--- classes/class_Level.py
+++ classes/class_Level.py
@@ -17,48 +17,48 @@ class Level:
         self.enemies = enemies
         self.end_level = False
 
-    def check_for_platform(self):
+    def check_for_platform(self, object):
         on_platform = False
         for platform in self.platforms:
             end_loop = False
             # check for platform on the left
-            if (platform.x + platform.width > self.player.x + self.player.vx > platform.x + platform.width / 2) and \
+            if (platform.x + platform.width > object.x + object.vx > platform.x + platform.width / 2) and \
                     (
-                            platform.y + platform.height > self.player.y > platform.y or
-                            platform.y + platform.height > self.player.y + self.player.height > platform.y):
+                            platform.y + platform.height > object.y > platform.y or
+                            platform.y + platform.height > object.y + object.height > platform.y):
                 end_loop = True
-                self.player.push_x = True
-                self.player.vx = (platform.x + platform.width) - self.player.x
+                object.push_x = True
+                object.vx = (platform.x + platform.width) - object.x
             # check for platform on the right
-            if (platform.x + platform.width / 2 > self.player.x + self.player.width + self.player.vx > platform.x) and \
+            if (platform.x + platform.width / 2 > object.x + object.width + object.vx > platform.x) and \
                     (
-                            platform.y + platform.height > self.player.y > platform.y or
-                            platform.y + platform.height > self.player.y + self.player.height > platform.y):
-                self.player.push_x = True
-                self.player.vx = (self.player.x + self.player.width) - platform.x
+                            platform.y + platform.height > object.y > platform.y or
+                            platform.y + platform.height > object.y + object.height > platform.y):
+                object.push_x = True
+                object.vx = (object.x + object.width) - platform.x
                 end_loop = True
             # check for platform below
-            if (platform.y + platform.height / 2 >= self.player.y + self.player.height + self.player.vy >=
+            if (platform.y + platform.height / 2 >= object.y + object.height + object.vy >=
                 platform.y) \
-                    and ((platform.x + platform.width >= self.player.x + self.player.vx >= platform.x) or
+                    and ((platform.x + platform.width >= object.x + object.vx >= platform.x) or
                          (
-                                 platform.x + platform.width >= self.player.x + self.player.width + self.player.vx >=
-                                 platform.x)) and self.player.vy >= 0:
+                                 platform.x + platform.width >= object.x + object.width + object.vx >=
+                                 platform.x)) and object.vy >= 0:
                 end_loop = True
                 on_platform = True
                 self.push_up(platform)
             # check for platform ahead
-            if not end_loop and (platform.y + platform.height / 2 < self.player.y + self.player.vy <
+            if not end_loop and (platform.y + platform.height / 2 < object.y + object.vy <
                                  platform.y + platform.height) \
-                    and ((platform.x + platform.width >= self.player.x + self.player.vx >= platform.x) or
+                    and ((platform.x + platform.width >= object.x + object.vx >= platform.x) or
                          (
-                                 platform.x + platform.width >= self.player.x + self.player.width + self.player.vx >=
-                                 platform.x)) and self.player.vy <= 0:
+                                 platform.x + platform.width >= object.x + object.width + object.vx >=
+                                 platform.x)) and object.vy <= 0:
                 end_loop = True
                 self.push_down(platform)
                 if end_loop:
                     break
-        self.player.on_platform = on_platform
+        object.on_platform = on_platform
 
     def push_up(self, platform):
         """This method is for preventing some bugs and push player straightly onto the platform"""
@@ -96,7 +96,7 @@ class Level:
 
     def game(self):
         """Main method of each level game, is called every 16 ms"""
-        self.check_for_platform()
+        self.check_for_platform(self.player)
         self.player.move()
         self.camera.update()
         if not self.check_for_end():
